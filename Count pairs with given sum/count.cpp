ecca6b4f711af0dec2f0ca857deba5b55618db77
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-02-16 23:25:36 +0530

    Create count.cpp

---
+++ Count pairs with given sum/count.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    int getPairsCount(int[] arr, int n, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(k-arr[i]))
+            {
+                count=count+map.get(k-arr[i]);
+                map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            }
+            else
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        return(count);
+    }
+}
