5d4317f8bf6e74773e2e615870026240e694f5fd
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-03-20 22:35:52 +0530

    Create code.cpp

---
+++ Reverse a linked list/code.cpp
@@ -0,0 +1,30 @@
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        struct Node* forward=head;
+        struct Node* curr=head;
+        struct Node* prev= NULL;
+        while(curr!= NULL){
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
+    
+};
+    
