5d425b186244b33d77c9ff2f22a71aec6bc7e306
Debanjan Guha Thakurata <91559219+debanjandgt@users.noreply.github.com>
2023-12-19 22:57:04 +0530

    Create code.cpp

---
+++ Rightmost different bit/code.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+       if(m==n) return -1;
+        int ans=1;
+        int num=m^n;
+        while(num>0){
+            if(num&1) return ans;
+            num=num>>1;
+            ans++;
+        }
+        
+        return ans;
+    }
+};
